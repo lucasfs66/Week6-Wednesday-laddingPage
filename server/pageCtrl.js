fa2597159817582146c0835b0e1e9ee404f0feeb
Lucas <lucasfreiresabino@gmail.com>
2022-11-02 15:08:03 -0600

    hello

--- server/pageCtrl.js
+++ server/pageCtrl.js
@@ -1,7 +1,10 @@
+
+
 const path = require("path")
+
 
 module.exports = {
     home: (req, res) => {
-        res.sendFile(path.join(__dirname, "../index.html"))
+        res.sendFile(path.join(__dirname, ".././index.html"))
     }
 }
